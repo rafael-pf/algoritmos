f2c2855c2407b3f5fd85650575d68d8c9530c643
Rafael Paz <132072483+rafael-pf@users.noreply.github.com>
2023-11-06 16:50:04 -0300

    Update AP1.c

--- APs/AP1.c
+++ APs/AP1.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void Merge(){
-
-
-
+void Merge(int *A, int *tmp, int l, int r){
+    int m = l+r/2;
+    int i1=l, i2=m+1;
+    for(int i=l; i<=r; i++){
+        tmp[i]=A[i];
+    }
+    for(int curr=l; curr<=r; curr++){
+        if(i1==m+1) {A[curr]=tmp[i2++];}
+        else if(i2>r) {A[curr]=tmp[i1++];}
+        else if(tmp[i1]<=tmp[i2]) {A[curr]=tmp[i1++];}
+        else {A[curr]=tmp[i2++];}
+    }
 }
 
-void MergeSort(int *A, int l, int r){
-    int m=0;
+void MergeSort(int *A, int *tmp, int l, int r){
     if(l<r){
-        
-        MergeSort();
-        MergeSort();
-        Merge();
+        int m = (l+r)/2;
+        MergeSort(A, tmp, l, m);
+        MergeSort(A, tmp, m+1, r);
+        Merge(A, tmp, l, r);
     }
-
 }
 
 int main(){
 
     int c, n, i=0, j=0;
-    int *vet=NULL;
     scanf("%d", &c);
     for(i=0; i<c; i++){
         scanf(" %d", &n);
-        vet = (int*) malloc(n*sizeof(int));
+        int vet[n], tmp[n];
         for(j=0; j<n; j++){
             scanf(" %d", &vet[j]);
         }
 
-        MergeSort(vet, tam);
+        MergeSort(vet, tmp, 0, n-1);
 
         for(j=0; j<n; j++){
             printf("%d ", vet[j]);
         }
-        free(vet);
     }
 
     return 0;
