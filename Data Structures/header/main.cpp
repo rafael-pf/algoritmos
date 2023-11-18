e41c27a198f5af2431c72c902715b583282ea4c4
Rafael Paz <132072483+rafael-pf@users.noreply.github.com>
2023-11-18 09:42:32 -0300

    Update main.cpp

--- Data Structures/header/main.cpp
+++ Data Structures/header/main.cpp
@@ -10,6 +10,15 @@ int main(){
     insert(l, 7);
     insert(l, 9);
     printList(l);
+    prev(l);
+    cout << "Removed item: " << remove(l) << "\n";
+    printList(l);
+    moveToStart(l);
+    insert(l, 0);
+    printList(l);
+    moveToEnd(l);
+    insert(l, 1);
+    printList(l);
 
 
     return 0;
