7a3710f2dc72cd717148d95a0874e7ba21714e51
Rafael <rafapazfernandes@gmail.com>
2024-02-26 21:36:35 -0300

    AP11 done

---
+++ APs/AP11.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int,int> Pair; 
+
+typedef struct{
+    int value;
+    int weight;
+}Edge;
+
+typedef struct{
+    vector<Edge> edges;
+    int value;
+}Vertice;
+
+typedef struct{
+    int numEdges;
+    int numVertices;
+    Vertice* vertices;
+    int* mark;
+}Graph;
+
+Graph* createGraph(int n){
+    Graph* g = new Graph;
+    g->mark = new int[n];
+    g->numEdges=0;
+    g->numVertices=n;
+    g->vertices=new Vertice[n];
+    return g;
+}
+
+void setEdge(Graph* g, int u, int v, int w){
+    g->vertices[u].edges.push_back({v, w});
+    g->vertices[v].edges.push_back({u, w});
+    g->numEdges++;
+}
+
+void setMark(Graph* g, int u, int value){
+    g->mark[u] = value;
+}
+
+int getMark(Graph* g, int u){
+    return g->mark[u];
+}
+
+int first(Graph* g, int u){
+    if(g->vertices[u].edges.empty()) return g->numVertices;
+    return g->vertices[u].edges.front().value;
+}
+
+int weight(Graph* g, int u, int v){
+    for(int i=0; i<g->vertices[u].edges.size(); i++){
+        if(g->vertices[u].edges[i].value==v){
+            return g->vertices[u].edges[i].weight;
+        }
+    }
+    return 0;
+}
+
+void dijkstra(Graph* g, int s, int d[]){
+    int v, w, count, peso;
+    for(int i=0; i<g->numVertices; i++){
+        d[i] = INT_MAX;
+        g->mark[i]=0;
+    }
+    priority_queue<Pair, vector<Pair>, greater<Pair>> heap;
+    heap.push({0, s});
+    d[s]=0;
+    for(int i=0; i<g->numVertices; i++){
+        do{
+            if(heap.empty()) return;  
+            v = heap.top().second;
+            heap.pop();
+        }while(getMark(g, v));
+        setMark(g, v, 1);
+        w = first(g, v);
+        count = 0;
+        while(w<g->numVertices){
+            if(!getMark(g, w)){
+                peso = weight(g, v, w);
+                if(d[w]>d[v]+peso){
+                    d[w]=d[v]+peso;
+                    heap.push({d[w], w});
+                }
+            }
+            if(count+1<g->vertices[v].edges.size()) count++;
+            else break;
+            w = g->vertices[v].edges[count].value;
+        }
+    }
+}
+
+int main(){
+
+    int n, m, v, a, b, w;
+    cin >> n >> m >> v;
+    int d[n];
+    Graph* g = createGraph(n);
+    for(int i=0; i<m; i++){
+        cin >> a >> b >> w;
+        setEdge(g, a, b, w);
+    }
+    dijkstra(g, v, d);
+    for(int i=0; i<n; i++){
+        cout << d[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
