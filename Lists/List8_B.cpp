4ae59501894202fb7d51e6a26f808483e17652e5
Rafael <rafapazfernandes@gmail.com>
2024-03-13 22:51:39 -0300

    sapo

---
+++ Lists/List8_B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int magia(int N, int K, int S[], int init, int end){
+}
+
+int main(){
+    int N, K;
+    cin >> N >> K;
+    int S[N];
+    // int **M
+    for(int i=0; i<N; i++){
+        cin >> S[i];
+    }
+
+    
+
+    return 0;
+}
